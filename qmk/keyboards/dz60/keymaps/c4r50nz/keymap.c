4b00111024425b543730512dab0fa973b3ae2cd4
Xiangyu Zhu <frefreak.zxy@gmail.com>
2018-12-11 18:22:21 +0800

    LT(1, KC_SPC)

--- qmk/keyboards/dz60/keymaps/c4r50nz/keymap.c
+++ qmk/keyboards/dz60/keymaps/c4r50nz/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
 		KC_CAPS, KC_A,    KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
 		KC_LSFT, KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-		KC_LCTL, KC_LGUI, KC_LALT,             KC_SPC,                       F(0),    F(1),    F(2),    KC_RCTL),
+		KC_LCTL, KC_LGUI, KC_LALT,             LT(1, KC_SPC),                F(1),    F(2),    F(3),    KC_RCTL),
 	// 1: Function Layer
 	LAYOUT_60_ansi(
 		KC_TRNS, KC_F1,   KC_F2,  KC_F3,   KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
@@ -51,4 +51,5 @@ const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_LAYER_MOMENTARY(1),  // to F1 overlay
     [1] = ACTION_LAYER_MOMENTARY(2),  // to F2 overlay
     [2] = ACTION_LAYER_MOMENTARY(3),  // to F3 overlay
+    [3] = ACTION_LAYER_MOMENTARY(4),  // to F4 overlay
 };
