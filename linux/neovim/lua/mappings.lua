221b9e07a93f279327976604a464ae26c646fb98
carsonzhu <carsonzhu@tencent.com>
2021-08-12 11:28:27 +0800

    add linux folder

---
+++ linux/neovim/lua/mappings.lua
@@ -0,0 +1,93 @@
+local function map(mode, lhs, rhs, opts)
+    local options = {noremap = true, silent = true}
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+local opt = {}
+
+map("n", "<leader>n", ":set nu!<CR>", opt)
+map("n", "<C-n>", ":NvimTreeToggle<CR>", opt)
+map("n", "<A-/>", ":CommentToggle<CR>", opt)
+map("v", "<A-/>", ":CommentToggle<CR>", opt)
+
+-- compe stuff
+local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+    local col = vim.fn.col(".") - 1
+    if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+        return true
+    else
+        return false
+    end
+end
+
+_G.tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-n>"
+    elseif check_back_space() then
+        return t "<Tab>"
+    else
+        return vim.fn["compe#complete"]()
+    end
+end
+
+_G.s_tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-p>"
+    elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+        return t "<Plug>(vsnip-jump-prev)"
+    else
+        return t "<S-Tab>"
+    end
+end
+
+function _G.completions()
+    local npairs
+    if
+        not pcall(
+            function()
+                npairs = require "nvim-autopairs"
+            end
+        )
+     then
+        return
+    end
+
+    if vim.fn.pumvisible() == 1 then
+        if vim.fn.complete_info()["selected"] ~= -1 then
+            return vim.fn["compe#confirm"]("<CR>")
+        end
+    end
+    return npairs.check_break_line_char()
+end
+
+--  compe mappings
+map("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+map("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+map("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+map("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+map("i", "<CR>", "v:lua.completions()", {expr = true})
+
+-- bufferline tab stuff
+map("n", "<S-t>", ":enew<CR>", opt) -- new buffer
+map("n", "<C-t>b", ":tabnew<CR>", opt) -- new tab
+map("n", "<S-x>", ":bd!<CR>", opt) -- close tab
+
+-- move between tabs
+map("n", "<TAB>", ":BufferLineCycleNext<CR>", opt)
+map("n", "<S-TAB>", ":BufferLineCyclePrev<CR>", opt)
+
+-- use ESC to turn off search highlighting
+map("n", "<Esc>", ":noh<CR>", opt)
+
+-- Vim Fugitive
+map("n", "<Leader>gs", ":Git<CR>", opt)
+map("n", "<Leader>gh", ":diffget //2<CR>", opt)
+map("n", "<Leader>gl", ":diffget //3<CR>", opt)
+map("n", "<Leader>gb", ":Git blame<CR>", opt)
---
+++ linux/neovim/lua/plugins/telescope-nvim.lua
@@ -0,0 +1,88 @@
+local telescope
+if
+    not pcall(
+        function()
+            telescope = require("telescope")
+        end
+    )
+ then
+    return
+end
+
+telescope.setup(
+    {
+        defaults = {
+            vimgrep_arguments = {
+                "rg",
+                "--color=never",
+                "--no-heading",
+                "--with-filename",
+                "--line-number",
+                "--column",
+                "--smart-case"
+            },
+            prompt_prefix = "   ",
+            selection_caret = "  ",
+            entry_prefix = "  ",
+            initial_mode = "insert",
+            selection_strategy = "reset",
+            sorting_strategy = "descending",
+            layout_strategy = "horizontal",
+            layout_config = {
+                horizontal = {
+                    prompt_position = "top",
+                    preview_width = 0.55,
+                    results_width = 0.8
+                },
+                vertical = {
+                    mirror = false
+                },
+                width = 0.87,
+                height = 0.80,
+                preview_cutoff = 120
+            },
+            file_sorter = require("telescope.sorters").get_fuzzy_file,
+            file_ignore_patterns = {},
+            generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+            path_display = {"absolute"},
+            winblend = 0,
+            border = {},
+            borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
+            color_devicons = true,
+            use_less = true,
+            set_env = {["COLORTERM"] = "truecolor"}, -- default = nil,
+            file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+            grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+            qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+            -- Developer configurations: Not meant for general override
+            buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker
+        },
+        extensions = {
+            fzf = {
+                fuzzy = true, -- false will only do exact matching
+                override_generic_sorter = false, -- override the generic sorter
+                override_file_sorter = true, -- override the file sorter
+                case_mode = "smart_case" -- or "ignore_case" or "respect_case"
+                -- the default case_mode is "smart_case"
+            },
+        }
+    }
+)
+
+local opt = {noremap = true, silent = false}
+
+vim.g.mapleader = " "
+
+-- mappings
+vim.api.nvim_set_keymap("n", "<Leader>ff", [[<Cmd>lua require('telescope.builtin').find_files()<CR>]], opt)
+
+vim.api.nvim_set_keymap("n", "<Leader>fb", [[<Cmd>lua require('telescope.builtin').buffers()<CR>]], opt)
+vim.api.nvim_set_keymap("n", "<Leader>fh", [[<Cmd>lua require('telescope.builtin').help_tags()<CR>]], opt)
+vim.api.nvim_set_keymap("n", "<Leader>fo", [[<Cmd>lua require('telescope.builtin').oldfiles()<CR>]], opt)
+
+-- dashboard stuff
+vim.api.nvim_set_keymap("n", "<Leader>fw", [[<Cmd> Telescope live_grep<CR>]], opt)
+vim.api.nvim_set_keymap("n", "<Leader>fn", [[<Cmd> DashboardNewFile<CR>]], opt)
+vim.api.nvim_set_keymap("n", "<Leader>bm", [[<Cmd> DashboardJumpMarks<CR>]], opt)
+vim.api.nvim_set_keymap("n", "<Leader>sl", [[<Cmd> SessionLoad<CR>]], opt)
+vim.api.nvim_set_keymap("n", "<Leader>ss", [[<Cmd> SessionSave<CR>]], opt)
