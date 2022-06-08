e4a026999e72aabab9150e2eac922736bbae6e19
Frefreak <frefreak.zxy@gmail.com>
2022-06-08 22:21:27 +0800

    update qmk conf

--- qmk/keyboards/ymdk/bface/keymaps/c4r50nz/keymap.c
+++ qmk/keyboards/ymdk/bface/keymaps/c4r50nz/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
 		KC_LCTL, KC_A,    KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
 		KC_LSFT, KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-		KC_CAPS, KC_LGUI, KC_LALT,             LT(1, KC_SPC),                F(1),    F(2),    F(3),    KC_RCTL),
+		KC_CAPS, KC_LGUI, KC_LALT,             LT(1, KC_SPC),                MO(2),    TG(3),    MO(4),    KC_RCTL),
 	// 1: Function Layer
 	LAYOUT_60_ansi(
 		KC_TRNS, KC_F1,   KC_F2,  KC_F3,   KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,
 		KC_LCTL,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,  KC_QUOT, KC_ENT,
 		KC_LSFT,  KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
-		KC_CAPS,  KC_LGUI,          KC_LALT,                KC_SPC,                    F(3), KC_TRNS, KC_TRNS, KC_TRNS),
+		KC_CAPS,  KC_LGUI,          KC_LALT,                KC_SPC,                    MO(4), KC_TRNS, KC_TRNS, KC_TRNS),
 	// 4: Function Layer
 	LAYOUT_60_ansi(
 		KC_TRNS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
@@ -51,12 +51,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS,  KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS,
 		KC_TRNS,  KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
 		KC_TRNS,  KC_TRNS,          KC_0,                KC_TRNS,                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
-};
-
-// Custom Actions
-const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_LAYER_MOMENTARY(1),  // to F1 overlay
-    [1] = ACTION_LAYER_MOMENTARY(2),  // to F2 overlay
-    [2] = ACTION_LAYER_TOGGLE(3),     // to F3 overlay
-    [3] = ACTION_LAYER_MOMENTARY(4),  // to F4 overlay
 };
