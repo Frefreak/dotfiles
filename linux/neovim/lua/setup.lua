0636f2a82c346a437ef204a86b91ac7f438c55bf
carsonzhu <carsonzhu@tencent.com>
2022-10-18 19:25:48 +0800

    update cmp_nvim_lsp config

--- linux/neovim/lua/setup.lua
+++ linux/neovim/lua/setup.lua
@@ -94,8 +94,7 @@ local on_attach = function(client, bufnr)
                    opts)
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local servers = {'pyright', 'tsserver', 'clangd', 'gopls'}
 for _, lsp in ipairs(servers) do
