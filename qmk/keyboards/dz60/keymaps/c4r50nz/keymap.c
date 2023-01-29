959d168584061e6eaae3dae275f97323a0ae3ddf
carsonzhu <carsonzhu@tencent.com>
2023-01-29 12:35:53 +0800

    small update

--- qmk/keyboards/dz60/keymaps/c4r50nz/keymap.c
+++ qmk/keyboards/dz60/keymaps/c4r50nz/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LCTL,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,  KC_QUOT, KC_ENT,
 		KC_LSFT,  KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
 		KC_CAPS,  KC_GRAVE,          KC_LALT,                KC_SPC,                    MO(4), KC_TRNS, MO(5), KC_TRNS),
-	// same as Layer 1
+	// almost same as Layer 1
 	// 4: Function Layer
 	LAYOUT_60_ansi(
 		KC_TRNS, KC_F1,   KC_F2,  KC_F3,   KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
