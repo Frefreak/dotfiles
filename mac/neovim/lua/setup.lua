ba663905d7ff85152456fe1aa9144279af5994dd
carsonzhu <carsonzhu@tencent.com>
2023-05-05 11:43:00 +0800

    update pyright lspconfig (remove detecting setup.py as root dir)

--- mac/neovim/lua/setup.lua
+++ mac/neovim/lua/setup.lua
@@ -1,5 +1,5 @@
 function map(mode, lhs, rhs, opts)
-    local options = {noremap = true, silent = true}
+    local options = { noremap = true, silent = true }
     if opts then options = vim.tbl_extend("force", options, opts) end
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
@@ -41,7 +41,7 @@ local nlspsettings = require('nlspsettings')
 nlspsettings.setup({
     config_home = vim.fn.stdpath('config') .. '/nlsp-settings',
     local_settings_dir = ".nlsp-settings",
-    local_settings_root_markers = {'.git'},
+    local_settings_root_markers = { '.git' },
     append_default_schemas = true,
     loader = 'json'
 })
@@ -55,13 +55,13 @@ local on_attach = function(client, bufnr)
     local function buf_set_option(...)
         vim.api.nvim_buf_set_option(bufnr, ...)
     end
-    require"lsp_signature".on_attach()
+    require "lsp_signature".on_attach()
 
     -- Enable completion triggered by <c-x><c-o>
     buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
     -- Mappings.
-    local opts = {noremap = true, silent = true}
+    local opts = { noremap = true, silent = true }
 
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
@@ -69,70 +69,86 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
     buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
     buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>',
-                   opts)
+        opts)
     buf_set_keymap('n', '<space>wa',
-                   '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+        '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
     buf_set_keymap('n', '<space>wr',
-                   '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+        '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
     buf_set_keymap('n', '<space>wl',
-                   '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
-                   opts)
+        '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+        opts)
     buf_set_keymap('n', '<space>D',
-                   '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+        '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
     buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
     buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',
-                   opts)
+        opts)
     buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
     buf_set_keymap('n', '<space>e',
-                   '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
-                   opts)
+        '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
+        opts)
     buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
     buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
     buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>',
-                   opts)
+        opts)
     buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.format({async = true})<CR>',
-                   opts)
+        opts)
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-local servers = {'pyright', 'tsserver', 'clangd', 'gopls', 'lua_ls', 'hls'}
+local servers = { 'pyright', 'tsserver', 'clangd', 'gopls', 'lua_ls', 'hls' }
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup({
         on_attach = on_attach,
         capabilities = capabilities,
-        flags = {debounce_text_changes = 150}
+        flags = { debounce_text_changes = 150 }
     })
 end
+
+local root_files = {
+  'pyproject.toml',
+  'setup.cfg',
+  'requirements.txt',
+  'Pipfile',
+  'pyrightconfig.json',
+}
+
+require('lspconfig').pyright.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    root_dir = require('lspconfig/util').root_pattern(unpack(root_files)),
+    flags = { debounce_text_changes = 150 }
+})
+
 
 local rt = require('rust-tools')
 rt.setup({
     tools = {
-        runnables = {use_telescope = true},
-        debuggables = {use_telescope = true},
-        inlay_hints = {highlight = "InLayHints", auto = true},
-        hover_actions = {auto_focus = true}
+        runnables = { use_telescope = true },
+        debuggables = { use_telescope = true },
+        inlay_hints = { highlight = "InLayHints", auto = true },
+        hover_actions = { auto_focus = true }
     },
     server = {
         on_attach = function(client, bufnr)
             on_attach(client, bufnr)
             vim.keymap.set("n", "<space>a", rt.hover_actions.hover_actions,
-                           {buffer = bufnr, silent = true, noremap = true})
+                { buffer = bufnr, silent = true, noremap = true })
         end,
         capabilities = capabilities,
-        flags = {debounce_text_changes = 150},
+        flags = { debounce_text_changes = 150 },
         settings = {}
     }
 })
 
 local util = require 'lspconfig/util'
-require"lspconfig".efm.setup {
-    init_options = {documentFormatting = true},
+require "lspconfig".efm.setup {
+    init_options = { documentFormatting = true },
     on_attach = on_attach,
     settings = {
-        rootMarkers = {".git/"},
+        rootMarkers = { ".git/" },
         languages = {
-            lua = {{formatCommand = "lua-format -i", formatStdin = true}},
+            lua = { { formatCommand = "lua-format -i", formatStdin = true } },
             python = {
                 {
                     formatCommand = "black --target-version py310 --quiet -",
@@ -141,17 +157,17 @@ require"lspconfig".efm.setup {
             }
         }
     },
-    filetypes = {"lua", "python"},
+    filetypes = { "lua", "python" },
     root_dir = function(fname)
         return util.root_pattern(".git")(fname) or vim.fn.getcwd()
     end
 }
 
 local home = os.getenv('HOME')
-require'lspconfig'.omnisharp.setup {
+require 'lspconfig'.omnisharp.setup {
     on_attach = on_attach,
     capabilities = capabilities,
-    flags = {debounce_text_changes = 150},
+    flags = { debounce_text_changes = 150 },
 
     cmd = { "dotnet", home .. "/omnisharp/OmniSharp.dll" },
 
@@ -196,7 +212,7 @@ vim.o.completeopt = 'menu,menuone,noselect'
 
 local cmp = require 'cmp'
 cmp.setup({
-    snippet = {expand = function(args) vim.fn["UltiSnips#Anon"](args.body) end},
+    snippet = { expand = function(args) vim.fn["UltiSnips#Anon"](args.body) end },
     mapping = {
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-n>'] = cmp.mapping.select_next_item(),
@@ -224,25 +240,24 @@ cmp.setup({
         end
     },
     sources = {
-        {name = 'nvim_lsp'}, {name = 'ultisnips'}, {name = 'buffer'},
-        {name = 'path'}, {name = 'nvim_lua'}
+        { name = 'nvim_lsp' }, { name = 'ultisnips' }, { name = 'buffer' },
+        { name = 'path' }, { name = 'nvim_lua' }
     },
     formatting = {
-        fields = {'abbr', 'kind', 'menu'},
+        fields = { 'abbr', 'kind', 'menu' },
         format = function(_, vim_item) return vim_item end
     }
-
 })
 
 -- lspkind
 local lspkind = require('lspkind')
-cmp.setup {formatting = {format = lspkind.cmp_format()}}
+cmp.setup { formatting = { format = lspkind.cmp_format() } }
 
 -- treesitter
 
 local ts_config = require('nvim-treesitter.configs')
 ts_config.setup {
-    highlight = {enable = true, use_languagetree = true},
+    highlight = { enable = true, use_languagetree = true },
 }
 
 -- telescope
