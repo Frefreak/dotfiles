e846f4d07464c1f48b029a2443b6dffa1f3c5d50
carsonzhu <carsonzhu@tencent.com>
2023-12-27 16:03:34 +0800

    re-enable ultisnips

--- linux/neovim/lua/setup.lua
+++ linux/neovim/lua/setup.lua
@@ -43,49 +43,49 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    if vim.bo.filetype ~= 'rust' then
-        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    end
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-  end,
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        if vim.bo.filetype ~= 'rust' then
+            vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+        end
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-local servers = { 'pyright', 'tsserver', 'clangd', 'gopls', 'lua_ls', 'hls'}
+local servers = { 'pyright', 'tsserver', 'clangd', 'gopls', 'lua_ls', 'hls' }
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {}
 end
 
 local root_files = {
-  'pyproject.toml',
-  'setup.cfg',
-  'requirements.txt',
-  'Pipfile',
-  'pyrightconfig.json',
+    'pyproject.toml',
+    'setup.cfg',
+    'requirements.txt',
+    'Pipfile',
+    'pyrightconfig.json',
 }
 
 require('lspconfig').pyright.setup({
@@ -167,7 +167,7 @@ vim.o.completeopt = 'menu,menuone,noselect'
 
 local cmp = require 'cmp'
 cmp.setup({
-    -- snippet = { expand = function(args) vim.fn["UltiSnips#Anon"](args.body) end },
+    snippet = { expand = function(args) vim.fn["UltiSnips#Anon"](args.body) end },
     mapping = {
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-n>'] = cmp.mapping.select_next_item(),
@@ -196,7 +196,7 @@ cmp.setup({
     },
     sources = {
         { name = 'nvim_lsp' },
-        -- { name = 'ultisnips' },
+        { name = 'ultisnips'},
         { name = 'buffer' },
         { name = 'path' }, { name = 'nvim_lua' }
     },
@@ -214,17 +214,17 @@ cmp.setup { formatting = { format = lspkind.cmp_format() } }
 -- rustaceanvim
 if vim.bo.filetype == 'rust' then
     vim.keymap.set(
-      {"n", "v"},
-      "<space>ca",
-      function()
-        vim.cmd.RustLsp('codeAction');
-      end,
-      { silent = true, buffer = bufnr }
+        { "n", "v" },
+        "<space>ca",
+        function()
+            vim.cmd.RustLsp('codeAction');
+        end,
+        { silent = true, buffer = bufnr }
     )
     vim.keymap.set('n', '<leader>rd', function()
-        vim.cmd.RustLsp('externalDocs');
-    end,
-      { silent = true, buffer = bufnr }
+            vim.cmd.RustLsp('externalDocs');
+        end,
+        { silent = true, buffer = bufnr }
     )
 end
 
