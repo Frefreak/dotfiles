3cb4d3f149c3677f545ea074c178fc101c3ad141
carsonzhu <carsonzhu@tencent.com>
2024-04-09 10:03:29 +0800

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -11,6 +11,7 @@ if status is-interactive
 
     set -x EDITOR nvim
     set -x MANPAGER "nvim +Man! -c 'call clearmatches()'"
+    set -g async_prompt_functions _pure_prompt_git
 
     fish_config theme choose Dracula
     set -x fish_key_bindings fish_hybrid_key_bindings
--- fish/functions/fish_hybrid_key_bindings.fish
+++ fish/functions/fish_hybrid_key_bindings.fish
@@ -1,11 +1,11 @@
-#vim: set ft=fish
-
 function fish_hybrid_key_bindings --description \
     "also use emacs keybinding in insert mode"
     fish_default_key_bindings -M insert
-    fish_vi_key_bindings --no-erase
+    fish_vi_key_bindings --no-erase insert
     bind -M insert -m default kj backward-char repaint
     bind -M replace -m default kj backward-char repaint
     bind -M default H beginning-of-line
     bind -M default L end-of-line
+    bind -M insert \cP history-prefix-search-backward
+    bind -M insert \cN history-prefix-search-forward
 end
