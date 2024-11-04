fc26f5feabe2640bfbd9fdd8a1a3d5240e8609dc
carsonzhu <carsonzhu@tencent.com>
2024-11-04 16:27:48 +0800

    temporarily fix rust-analyzer related annoyances

--- linux/neovim/lua/setup.lua
+++ linux/neovim/lua/setup.lua
@@ -75,7 +75,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-local servers = { 'ruff', 'pyright', 'tsserver', 'clangd', 'gopls', 'lua_ls', 'hls', 'typst_lsp', 'zls' }
+local servers = { 'ruff', 'ts_ls', 'clangd', 'gopls', 'lua_ls', 'hls', 'typst_lsp', 'zls' }
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {}
 end
@@ -236,6 +236,11 @@ vim.api.nvim_create_autocmd("BufReadPre", {
         )
     end,
 })
+vim.g.rustaceanvim = {
+   server = {
+     capabilities = vim.lsp.protocol.make_client_capabilities(),
+   }
+}
 
 -- treesitter
 
@@ -285,3 +290,14 @@ require("lualine").setup({
 
 -- navbuddy
 map('n', '<leader>b', ':Navbuddy<CR>', {})
+
+-- workaround
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
