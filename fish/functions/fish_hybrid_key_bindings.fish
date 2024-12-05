43a1a7cf54d8c6596ca4ca1ab3c505f4876dc246
carsonzhu <carsonzhu@tencent.com>
2024-12-05 11:05:15 +0800

    modify fish config

--- fish/functions/fish_hybrid_key_bindings.fish
+++ fish/functions/fish_hybrid_key_bindings.fish
@@ -1,26 +1,3 @@
-function __fish_cancel_commandline
-    # Close the pager if it's open (#4298)
-    commandline -f cancel
-
-    set -l cmd (commandline)
-    if test -n "$cmd"
-        commandline -C 1000000
-        if set -q fish_color_cancel
-            echo -ns (set_color $fish_color_cancel) "^C" (set_color normal)
-        else
-            echo -ns "^C"
-        end
-        if command -sq tput
-            # Clear to EOL (to erase any autosuggestion).
-            echo -n (tput el; or tput ce)
-        end
-        for i in (seq (commandline -L))
-            echo ""
-        end
-        commandline ""
-        commandline -f execute
-    end
-end
 
 function fish_hybrid_key_bindings --description \
     "also use emacs keybinding in insert mode"
@@ -32,8 +9,7 @@ function fish_hybrid_key_bindings --description \
     bind -M default L end-of-line
     bind -M insert \cP history-prefix-search-backward
     bind -M insert \cN history-prefix-search-forward
-    bind --erase --preset f,1
-    bind --erase --preset \cC
-    bind -M insert \cC __fish_cancel_commandline
-
+    bind -M insert \e/ history-token-search-forward
+    # bind -M insert \cC __fish_cancel_commandline
+    # bind -M default \cC __fish_cancel_commandline
 end
