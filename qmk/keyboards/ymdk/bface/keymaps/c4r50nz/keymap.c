350aa7456dc8de16aa7df440586dc78ab2382335
Frefreak <frefreak.zxy@gmail.com>
2025-05-18 12:37:39 +0800

    adjust ymdk

--- qmk/keyboards/ymdk/bface/keymaps/c4r50nz/keymap.c
+++ qmk/keyboards/ymdk/bface/keymaps/c4r50nz/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
 		KC_LCTL, KC_A,    KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
 		KC_LSFT, KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-		KC_CAPS, KC_LGUI, KC_LALT,             LT(1, KC_SPC),                MO(2),    TG(3),    MO(4),    KC_RCTL),
+		KC_CAPS, KC_LGUI, KC_LALT,             LT(1, KC_SPC),                MO(2),    TG(3),    MO(4),    TG(7)),
 	// 1: Function Layer
 	LAYOUT_60_ansi(
 		KC_TRNS, KC_F1,   KC_F2,  KC_F3,   KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
@@ -43,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,   KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,
 		KC_LCTL,  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,  KC_QUOT, KC_ENT,
 		KC_LSFT,  KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
-		KC_CAPS,  KC_GRAVE,          KC_LALT,                KC_SPC,                    MO(4), KC_TRNS, MO(5), KC_TRNS),
-	// same as Layer 1
+		KC_CAPS,  KC_GRAVE,          KC_LALT,                KC_SPC,                    MO(4), KC_TRNS, MO(5), TG(6)),
+	// same as Layer 1 except F keys
 	// 4: Function Layer
 	LAYOUT_60_ansi(
 		KC_TRNS, KC_F1,   KC_F2,  KC_F3,   KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
@@ -55,8 +55,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	// 5: F-keys
 	LAYOUT_60_ansi(
 		KC_TRNS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,
-		KC_TRNS,  KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS,  KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS,
+		KC_TRNS,  KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,    KC_TRNS,   KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS,  KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN,    KC_RIGHT,   KC_TRNS,  KC_TRNS, KC_TRNS,
 		KC_TRNS,  KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
-		KC_TRNS,  KC_TRNS,          KC_0,                KC_TRNS,                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+		KC_TRNS,  KC_TRNS,          KC_0,                KC_SPC,                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+	// 6: 泡泡堂
+	LAYOUT_60_ansi(
+		KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+		KC_TRNS,  KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS,
+		KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+		KC_TRNS,  KC_TRNS,          KC_TRNS,                KC_SPC,                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+    // 7: city car driving
+	LAYOUT_60_ansi(
+		TD(TD_GRAVE_ESC), KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+		KC_TAB,  KC_Q,    KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_UP,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+		KC_LCTL, KC_A,    KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_LEFT,  KC_DOWN,    KC_RIGHT,    KC_SCLN, KC_QUOT, KC_ENT,
+		KC_LSFT, KC_Z,    KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+		KC_CAPS, KC_LGUI, KC_LALT,             KC_SPC,                KC_RALT,    KC_TRNS,    KC_RGUI,    TG(7)),
 };
